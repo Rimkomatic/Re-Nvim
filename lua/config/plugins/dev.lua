2943497abe8617acddfdccbe39590e68b38f640f
Rik <96734674+Rimkomatic@users.noreply.github.com>
2025-03-27 22:25:55 +0530

    Update dev.lua

--- lua/config/plugins/dev.lua
+++ lua/config/plugins/dev.lua
@@ -1,14 +1,3 @@
 return {
-  {
-    dir = "~/workfiles/present.nvim/",
-    config = function()
-      --require "present"
-    end
-  },
-  -- {
-  --   dir = "~/workfiles/switchscheme.nvim/",
-  --   config = function()
-  --     require "switchscheme"
-  --   end
-  -- },
+
 }
